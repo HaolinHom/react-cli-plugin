d3fc54a67d6528650c008df6c9b7a1cffaabff3c
HaolinHom <haolinhom@gmail.com>
2021-06-15 14:44:37 +0800

    fix: remove publish.js;

--- publish.js
+++
@@ -1,8 +0,0 @@
-process.env.NODE_ENV = 'production';
-
-module.exports = async function (context, args) {
-  const { std } = context.utils;
-
-  // TODO: do something about publish!
-  std.warn('TODO: do something about publish!');
-};
